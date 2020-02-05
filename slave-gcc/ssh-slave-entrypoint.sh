fb386a14a49fd0e732741843dfd588c05b6656d0
Nhi TRAN <nntran@users.noreply.github.com>
2020-02-05 10:44:44 +0100

    Update ssh-slave-entrypoint.sh

--- slave-gcc/ssh-slave-entrypoint.sh
+++ slave-gcc/ssh-slave-entrypoint.sh
@@ -4,7 +4,6 @@ if [ ! -f "/etc/ssh/ssh_host_rsa_key" ] ; then
     ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 fi
 
-
 if [ ! -f "/etc/ssh/ssh_host_dsa_key" ] ; then
     ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 fi 
